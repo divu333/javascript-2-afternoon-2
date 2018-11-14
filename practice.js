12f18a1ae699d3fd7522cfc4282963ca8a990995
Divya <divu333@gmail.com>
2018-11-14 13:22:40 -0700

    Update practice.js
    not finished

--- practice.js
+++ practice.js
@@ -18,7 +18,10 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+function first (arr)
+{
+  return (arr[0]);
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -34,7 +37,9 @@ var arr = [40,50,60];
 
 //Code Here
 
-
+function last (arr){
+  return (arr[2]);
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -49,8 +54,13 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 
 //Code Here
 
+function looper(family){
+  for(i=0;i<family.length;i++){
+    alert(family[i]);
+  }
+}
 
-
+looper(family);
 ////////// PROBLEM 4 //////////
 
 // Do not edit the code below.
@@ -64,7 +74,13 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 
 //Code Here
 
-
+function reversedLooper(letters){
+  for(i=letters.length-1;i>=0;i--)
+  {
+    alert(letters[i]);
+  }
+}
+reversedLooper(letters);
 
 ////////// PROBLEM 5 //////////
 
@@ -80,7 +96,23 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 //Code Here
 
 
+function evenFinder(nums){
+  var newArr =[];
+for(i=0;i<nums.length;i++){
 
+  if(nums[i]%2===0){
+
+   newArr.push(nums[i]) ;
+
+  }  
+  
+}
+return newArr;
+
+
+}
+
+evenFinder(nums);
 
 
 
@@ -107,7 +139,27 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 
 //Code Here
 
+function divider(numbersArray){
+  var newArr =[[],[]];
 
+for(i=0;i<numbersArray.length;i++){
+
+if(numbersArray[i]%2===0){
+ 
+  newArr[0].push(numbersArray[i]) ;
+
+}  
+ else{
+
+     newArr[1].push(numbersArray[i]) ;
+
+}
+}
+return newArr;
+
+}
+
+divider(numbersArray);
 
 ////////// PROBLEM 7 //////////
 
@@ -129,6 +181,23 @@ var getRandomArbitrary = function() {
 //Code Here
 
 
+function finder(arr) {
+
+  var randomNum = getRandomArbitrary();
+  
+    for(i=0;i<arr.length;i++){
+  
+       if(arr[i]===randomNum){
+         
+         return true;
+       }
+      
+  }
+  return false;
+  
+  }
+  finder(numbers);
+  
 
 ////////// PROBLEM 8 //////////
 
@@ -156,6 +225,29 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 */
 
 //Code Here
+
+function removeItem(myGroceryList,removeList){
+  for(i=0;i<myGroceryList.length;i++){
+    if(myGroceryList[i]===removeList) {
+       myGroceryList.splice(i, 1);
+    }
+  }
+  return myGroceryList;
+
+}
+removeItem(myGroceryList,'MtnDew');
+
+
+function addItem(myGroceryList,addList){
+  for(i=0;i<myGroceryList.length;i++){
+      if(myGroceryList[i]!==addList) {
+         myGroceryList.push(addList);
+      }
+               return myGroceryList;
+    }
+  }
+  addItem(myGroceryList,'water');
+
 
 
 
